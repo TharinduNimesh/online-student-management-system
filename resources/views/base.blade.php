9dd6a3708b98d9466e160b7198dbb1b2fb74c208
Tharindu Nimesh <127307898+TharinduNimesh@users.noreply.github.com>
2023-08-11 12:32:13 +0530

    Initial commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/img/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/styles/bootstrap.css" />
+    <link rel="stylesheet" href="/styles/main.css" />
+    <title>
+        @yield('title') | SchoolPlus
+    </title>
+</head>
+
+<body>
+    <div class="container-fluid p-0">
+        @yield('content')
+    </div>
+</body>
+</html>
