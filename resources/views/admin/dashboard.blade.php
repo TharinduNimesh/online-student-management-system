9f183abfd48c5e721d1936e69ba833787b0f76cc
Tharindu Nimesh <127307898+TharinduNimesh@users.noreply.github.com>
2023-08-14 02:36:13 +0530

    recently joined teachers and officers on dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('section')
+    {{-- Summary Start --}}
     <div class="row">
         <div class="summary-box col-12 col-md-6 col-xl-3 px-3 py-2 bg-dark">
             <div class="row h-100">
@@ -49,7 +50,71 @@
             </div>
         </div>
     </div>
-    <div class="row">
-        
+    {{-- Summary End --}}
+
+    @php
+        $faker = Faker\Factory::create();
+    @endphp
+
+    {{-- Recent Joined Start --}}
+    <div class="row mt-4">
+        <div class="col-lg-6 mb-3 mb-lg-0 px-1">
+            <div class="p-3 bg-dark rounded">
+                <div class="w-100 mb-3 d-flex justify-content-between align-items-center px-2">
+                    <h5 class="text-secondary mx-3">Recently Joined Teachers</h5> 
+                    <a href="#" class="btn btn-danger d-none d-md-block">Show All</a>                   
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-dark">
+                        <thead>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Joined At</th>
+                        </thead>
+                        <tbody>
+                            @for ($i = 0; $i < 5; $i++)
+                                <tr>
+                                    <td>{{ $i + 1 }}</td>
+                                    <td>{{ $faker->name }}</td>
+                                    <td>{{ $faker->email }}</td>
+                                    <td>{{ $faker->date }}</td>
+                                </tr>
+                            @endfor
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 mb-3 mb-lg-0 px-1">
+            <div class="p-3 bg-dark rounded">
+                <div class="w-100 mb-3 d-flex justify-content-between align-items-center px-2">
+                    <h5 class="text-secondary mx-3">Recently Joined Officers</h5> 
+                    <a href="#" class="btn btn-danger d-none d-md-block">Show All</a>                   
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-dark">
+                        <thead>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Joined At</th>
+                        </thead>
+                        <tbody>
+                            @for ($i = 0; $i < 5; $i++)
+                                <tr>
+                                    <td>{{ $i + 1 }}</td>
+                                    <td>{{ $faker->name }}</td>
+                                    <td>{{ $faker->email }}</td>
+                                    <td>{{ $faker->date }}</td>
+                                </tr>
+                            @endfor
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
+    {{-- Recently Joined End --}}
 @endsection
